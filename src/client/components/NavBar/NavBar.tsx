70949f8bad4691fb4b9dd793dd0a76b4c063f921
Misaia2025 <ia.proyecto.2025.es@gmail.com>
2025-06-10 13:45:04 -0300

    Auto-commit: 2025-06-10 13:45:04

--- src/client/components/NavBar/NavBar.tsx
+++ src/client/components/NavBar/NavBar.tsx
@@ -32,7 +32,7 @@ export default function AppNavBar({ navigationItems }: { navigationItems: Naviga
           !isLandingPage,
       })}
     >
-      {isLandingPage && <Announcement />}
+      {/* {isLandingPage && <Announcement />} */}
       <nav className='flex items-center justify-between p-6 lg:px-8' aria-label='Global'>
         <div className='flex items-center lg:flex-1'>
           <WaspRouterLink
